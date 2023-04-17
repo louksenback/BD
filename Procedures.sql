587bb7ff5347c3363f79676e19ddc710268ca3b5
louksenback <79393286+louksenback@users.noreply.github.com>
2023-04-17 08:52:32 +0300

    Update Procedures.sql

--- Procedures.sql
+++ Procedures.sql
@@ -30,3 +30,59 @@ Begin
 	close first_record;
 End$
 delimiter ;
+
+
+
+
+
+/*PROCEDURE 2*/
+
+
+
+
+
+
+
+
+
+
+
+/*PROCEDURE 3*/
+
+
+delimiter $
+
+drop procedure if exists delete_If_Administrative$
+create procedure delete_If_Administrative(IN name varchar(20), IN lname varchar(20))
+
+Begin
+
+	declare flag int;
+	declare Adm_id char(10);
+	declare delete_Adm cursor for 
+
+	select * from worker inner join admin on wrk_AT = adm_AT where wrk_name = name AND wrk_lname = lname;
+
+	declare continue handler for not found set flag = 1;
+	Open delete_Adm;
+	set flag = 0;
+
+	repeat
+
+		Fetch delete_Adm into Adm_id;
+		if(flag = 0) then
+			if(adm_type = 'ADMINISTRATIVE') then
+				delete from worker where wrk_name = name AND wrk_lname = lname;
+			End if;
+		End if;
+
+	until (flag = 1)
+
+	End repeat;
+
+	close delete_Adm;
+
+
+
+End$
+delimiter ;
