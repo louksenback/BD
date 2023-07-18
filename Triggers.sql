cee8ae223f9207285fdcd8c5a58524a293d821bd
louksenback <79393286+louksenback@users.noreply.github.com>
2023-07-18 13:14:17 +0300

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -54,3 +54,62 @@ Begin
 	insert into Log values (CURRENT_TIMESTAMP, 'Insert', 'Destination', 1);
 End$
 delimiter ;
+
+
+
+/* Updates */
+
+delimiter $
+create trigger keep_Log_Update_trip
+after update on trip
+for each row
+Begin
+	insert into Log values (CURRENT_TIMESTAMP, 'Update', 'Trip', 1);
+End$
+delimiter ;
+
+
+
+
+
+delimiter $
+create trigger keep_Log_Update_reservation
+after update on reservation
+for each row
+Begin
+	insert into Log values (CURRENT_TIMESTAMP, 'Update', 'Reservation', 1);
+End$
+delimiter ;
+
+
+delimiter $
+create trigger keep_Log_Update_event
+after update on event
+for each row
+Begin
+	insert into Log values (CURRENT_TIMESTAMP, 'Update', 'Event', 1);
+End$
+delimiter ;
+
+
+delimiter $
+create trigger keep_Log_Update_travel_to
+after update on travel_to
+for each row
+Begin
+	insert into Log values (CURRENT_TIMESTAMP, 'Update', 'Travel_to', 1);
+End$
+delimiter ;
+
+
+delimiter $
+create trigger keep_Log_Update_destination
+after update on destination
+for each row
+Begin
+	insert into Log values (CURRENT_TIMESTAMP, 'Update', 'Destination', 1);
+End$
+delimiter ;
+
+
+
