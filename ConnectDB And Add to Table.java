d6f82a609a45ebf4bfc9bef4cd610db565377f44
louksenback <79393286+louksenback@users.noreply.github.com>
2023-07-26 23:13:40 +0300

    Create ConnectDB And Add to Table.java

---
+++ ConnectDB And Add to Table.java
@@ -0,0 +1,40 @@
+//Database Connection
+        
+        try{
+            //open connection
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project_bd_2022_2023", "root", "root");
+            //username is root and password is root
+            
+            
+            
+            Statement st = con.createStatement();
+            //mysql query
+            String sql = "select * from log";
+            ResultSet rs = st.executeQuery(sql);
+            
+            
+            while(rs.next()){
+                //data will be added until finish
+                String date = String.valueOf(rs.getDate("date_of_event"));
+                String event_type = String.valueOf(rs.getString("event_type"));
+                String table_name = String.valueOf(rs.getString("table_name"));
+                String user_id = String.valueOf(rs.getInt("user_id"));
+                
+                //String array for store data into jtable
+                String tbData[] = {date,event_type,table_name,user_id};
+                DefaultTableModel tblModel = (DefaultTableModel)tableLog.getModel();
+                
+                //Add string array data into jtable
+                tblModel.addRow(tbData);
+                
+                
+                
+            }
+            
+            
+            
+            con.close();
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println(e.getMessage());    
+        }
