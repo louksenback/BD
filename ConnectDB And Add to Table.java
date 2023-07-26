e273adc228d9377b85d0259108d34d8fc097388b
louksenback <79393286+louksenback@users.noreply.github.com>
2023-07-26 23:15:04 +0300

    Update ConnectDB And Add to Table.java

--- ConnectDB And Add to Table.java
+++ ConnectDB And Add to Table.java
@@ -1,3 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+
+
 //Database Connection
         
         try{
